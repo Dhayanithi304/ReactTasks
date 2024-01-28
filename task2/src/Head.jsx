ce32fc2b81e2049562fdbf19aa58a9647e8d07ef
dhayanithi304 <dhayanithi1330@gmail.com>
2024-01-29 02:30:39 +0530

    modified

--- task2/src/Head.jsx
+++ task2/src/Head.jsx
@@ -1,30 +1,29 @@
-import { useState } from "react";
-import React from 'react'
+import React from "react";
 
-export function Head() {
-    
-  const [isClicked, setIsClicked] = useState(true);
-  const [count, setcount] = useState(0);
+function Head() {
+  // let [isClicked, setIsClicked] = useState(true);
+  const isClicked = true; 
+  let [count, setcount] = useState(0);
 
-  function countIncrease() {
-    console.log('clicked');
+  const countIncrease = ()=> {
+    console.log("clicked");
     setcount(count + 1);
     setIsClicked(false);
-  };
+  }
 
-  const countdecrease = () => {
-    console.log('clicked');
+  const countdecrease = ()=> {
+    console.log("clicked");
     setcount(count - 1);
     setIsClicked(true);
   };
 
   return (
     <div>
-        <div>
-          <h5>cart {count}</h5>
-        </div>
+      <div>
+        <h5>cart {count}</h5>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Head
+export default Head;
--- task2/src/Home.jsx
+++ task2/src/Home.jsx
@@ -1,28 +1,42 @@
-// import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
-function Home(props) {
-    let isClicked = props.isClicked 
+function Home() {
+  let [isClicked, setIsClicked] = useState(true);
+  let [count, setcount] = useState(0);
+
+  const countIncrease = () => {
+    console.log("clicked");
+    setcount(count + 1);
+    setIsClicked(false);
+  };
+
+  const countdecrease = () => {
+    console.log("clicked");
+    setcount(count - 1);
+    setIsClicked(true);
+  };
+
   return (
-    <>
-      <div className="card">
-        <img src="#" className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{props.name}</h5>
-          <i className={props.className}></i>
-          <i className={props.className}></i>
-          <i className={props.className}></i>
-          <i className={props.className}></i>
-          <i className={props.className}></i>
-          <p className="card-text">{props.price}</p>
-          <p className="card-text">{props.rating}</p>
-          {isClicked ? 
-            <button onClick={props.countIncrease}>Add to cart</button>
-           : 
-            <button onClick={props.countdecrease}>Remove form cart</button>
-          }
-        </div>
+    <div>
+      <div>
+        <h6>Cart {count}</h6>
       </div>
-    </>
+        <div className="card">
+          <img src="#" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title"></h5>
+            <i className="fa fa-star"></i>
+            <p className="card-text">{item.price}</p>
+            <p className="card-text">edsyhga</p>
+            {isClicked ? (
+              <button onClick={countIncrease}>Add to cart</button>
+            ) : (
+              <button onClick={countdecrease}>Remove form cart</button>
+            )}
+          </div>
+        </div>
+    </div>
   );
 }
 
