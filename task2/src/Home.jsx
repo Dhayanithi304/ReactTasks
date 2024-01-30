d0a160bebc9ddf4d6b1069b752cc4484f414f09d
dhayanithi304 <dhayanithi1330@gmail.com>
2024-01-31 03:32:08 +0530

    modified

--- task2/src/Home.jsx
+++ task2/src/Home.jsx
@@ -18,11 +18,10 @@ function Home() {
     }
   };
 
-const handleClear = () => {
-  setcount(count = 0);
-  setIsClicked(isClicked = []);
-}
-
+  const handleClear = () => {
+    setcount(0);
+    setIsClicked([]);
+  };
 
   const data = [
     {
@@ -30,6 +29,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgbdZoNm_vkYeRAU2_DPUZ7gsgZoyr7YZPBQ&usqp=CAU",
       name: "Rolls-Royce Boat Tail",
+      star: 5,
       price: "$26.2 million",
     },
     {
@@ -37,6 +37,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpGJj9K8OZa82Zwvc1PjGzsL5FGjIRGZ2aJw&usqp=CAU",
       name: "Bugatti la Voiture Noire",
+      star: 5,
       price: "$18.1 million",
     },
     {
@@ -44,6 +45,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlsta0xROot7IgSMF4qqcsXtjycIAGC5hKzQ&usqp=CAU",
       name: "Pagani Zonda HP Barchetta",
+      star: 5,
       price: "$17.9 million",
     },
     {
@@ -51,6 +53,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkskzFPZBIVd3VulP5PQHE96SYu-Hyr9IdWg&usqp=CAU",
       name: "Rolls-Royce Sweptail",
+      star: 5,
       price: "$13.2 million",
     },
     {
@@ -58,6 +61,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1mlotylo-QCTEiX4kuQ7lePRxH82BNTxq4w&usqp=CAU",
       name: "Bugatti Centodieci",
+      star: 5,
       price: "$9.1 million",
     },
     {
@@ -65,6 +69,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrT1THR9cJc6e1ni8qLbU-XrqKBd2cwseNQ&usqp=CAU",
       name: "Lamborghini Veneno",
+      star: 5,
       price: "$8.3 million",
     },
     {
@@ -72,6 +77,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTme3DE9lEm2XfQKMyzPshvU3GpPloJKJmazA&usqp=CAU",
       name: "Maybach Exelero",
+      star: 4,
       price: "$8 million",
     },
     {
@@ -79,6 +85,7 @@ const handleClear = () => {
       imgUrl:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx3ZpXz_62RFVivj64Zcegmht2PzAmzfKobg&usqp=CAU",
       name: "Bugatti Divo",
+      star: 3,
       price: "$5.7 million",
     },
   ];
@@ -90,41 +97,91 @@ const handleClear = () => {
           <h1>Luxury Cars</h1>
         </div>
         <div className="count-area">
-          <i className="fa fa-cart-shopping"></i><span className="cart-count">Cart {count}</span>
+          <i className="fa fa-cart-shopping"></i>
+          <span className="cart-count">Cart {count}</span>
         </div>
       </header>
 
       <div className="container">
         <div className="clearBtn-div">
-          <button onClick="handleClear" >Clear All</button>
+          <button onClick={handleClear} className="clrBtn">Clear All</button>
         </div>
-        <div className="card-group">
+        <div id="card-group">
           {data.map((items) => (
-          <div className="card" key={items.id}>
-            <div className="img-div">
-            <img src={items.imgUrl} className="card-img-top"/>
+            <div className="card" key={items.id}>
+              <div className="img-div">
+                <img src={items.imgUrl} className="card-img-top" />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{items.name}</h5>
+                {items.star > 0 ? (
+                  items.star == 1 ? (
+                    <i className="fa fa-star"></i>
+                  ) : (
+                    ""
+                  )
+                ) : (
+                  ""
+                )}
+                {items.star == 2 ? (
+                  <i className="fa fa-star">
+                    <i className="fa fa-star"></i>
+                  </i>
+                ) : (
+                  ""
+                )}
+                {items.star == 3 ? (
+                  <i className="fa fa-star">
+                    {" "}
+                    <i className="fa fa-star"></i>{" "}
+                    <i className="fa fa-star"></i>
+                  </i>
+                ) : (
+                  ""
+                )}
+                {items.star == 4 ? (
+                  <i className="fa fa-star">
+                    <i className="fa fa-star"></i>{" "}
+                    <i className="fa fa-star"></i>{" "}
+                    <i className="fa fa-star"></i>
+                  </i>
+                ) : (
+                  ""
+                )}
+                {items.star == 5 ? (
+                  <i className="fa fa-star">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>{" "}
+                    <i className="fa fa-star"></i>{" "}
+                    <i className="fa fa-star"></i>
+                  </i>
+                ) : (
+                  ""
+                )}
+                <p className="card-text">{items.price}</p>
+                {!isClicked.includes(items.id) ? (
+                  <button
+                    onClick={() => countIncrease(items.id)}
+                    className="addBtn"
+                  >
+                    Add to cart
+                  </button>
+                ) : (
+                  ""
+                )}
+                {isClicked.includes(items.id) ? (
+                  <button
+                    onClick={() => countdecrease(items.id)}
+                    className="removeBtn"
+                  >
+                    Remove
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
-            <div className="card-body">
-              <h5 className="card-title">{items.name}</h5>
-              <i className="fa fa-star"></i>
-              <i className="fa fa-star"></i>
-              <p className="card-text">{items.price}</p>
-              <p className="card-text">edsyhga</p>
-              {!isClicked.includes(items.id) ? (
-                <button onClick={() => countIncrease(items.id)} className="addBtn">
-                  Add to cart
-                </button>
-              ) : (
-                ""
-              )}
-              {isClicked.includes(items.id) ? (
-                <button onClick={() => countdecrease(items.id)} className="removeBtn">Remove</button>
-              ) : (
-                ""
-              )}
-            </div>
-          </div>
-        ))}
+          ))}
         </div>
       </div>
     </>
